c6496a2e339cb0937c433ff03a6f1619e67b698b
Aditya Thakare <67742452+Fallenzero72@users.noreply.github.com>
2024-05-06 11:24:16 +0530

    Add files via upload

---
+++ Sandip_Foundation_B1/show1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def square(num):
+    sq = num * num
+    return sq
---
+++ Sandip_Foundation_B2/temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def fact(n):
+    fact = 1
+    for x in range(1,n+1):
+        fact = fact * x
+    return fact
